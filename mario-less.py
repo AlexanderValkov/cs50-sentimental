ac54825ee51cb0fe888af15b27de4607be8c2f1d
Alexander Valkov <none@nowhe.re>
2017-12-23 04:38:02 +0300

    mario (both) with command line arguments

---
+++ mario-less.py
@@ -0,0 +1,26 @@
+from sys import argv
+
+def main():
+    # check for argument
+    if len(argv) != 2:
+        print("Usage: python3.6", argv[0], "number")
+        exit()
+
+    # check that argument is int
+    try:
+        num = int(argv[1])
+    except ValueError:
+        print(argv[1], "is not a number")
+        exit()
+
+    # printing
+    for i in range(num):
+        j = num - i - 1
+        for k in range(j):
+            print(f" ", end="")
+        for k in range(i + 1):
+            print(f"#", end="")
+        print("#")
+        
+if __name__ == "__main__":
+    main()
