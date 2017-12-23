6eca742bdac35f2fe04967dafa7023c7f2c63102
Alexander Valkov <none@nowhe.re>
2017-12-23 04:51:05 +0300

    mario (both) prompt added

--- mario-more.py
+++ mario-more.py
@@ -1,17 +1,13 @@
 from sys import argv
 
 def main():
-    # check for argument
-    if len(argv) != 2:
-        print("Usage: python3.6", argv[0], "number")
-        exit()
-
-    # check that argument is int
-    try:
-        num = int(argv[1])
-    except ValueError:
-        print(argv[1], "is not a number")
-        exit()
+    while True:
+        try:
+            num = int(input("Height: "))
+            if num > 0 and num < 23:
+                break
+        except ValueError:
+            pass
 
     # printing
     for i in range(num):
